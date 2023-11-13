5891d588128347f2df358720de797f1619031d2d
VadimMaleev <maleeffset@yandex.by>
2023-11-13 09:27:47 +0300

    add post entity

--- src/repositories/posts/post.entity.ts
+++ src/repositories/posts/post.entity.ts
@@ -1,6 +1,28 @@
-import {Entity} from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity("Post")
 export class Post {
+  @PrimaryColumn({ type: "uuid" })
+  id: string;
 
+  @Column()
+  title: string;
+
+  @Column()
+  shortDescription: string;
+
+  @Column()
+  content: string;
+
+  @Column({ type: "uuid" })
+  blogId: string;
+
+  @Column()
+  blogName: string;
+
+  @Column({ type: "timestamp with time zone" })
+  createdAt: Date;
+
+  @Column()
+  isVisible: boolean;
 }
